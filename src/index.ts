b8c655fbbd8c2811fb6e1d94de49faa6bc04b740
simnJS <86597303+simnJS@users.noreply.github.com>
2022-06-22 18:52:31 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,6 @@ function connectDB() {
   mongoose.connect(`${process.env.DATABASE_URL}`, {
     serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 100000, // Close sockets after 45 seconds of inactivity  //45000
-    family: 4, // Use IPv4, skip trying IPv6
-    
   });
 }
 
