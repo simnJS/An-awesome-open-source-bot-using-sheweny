e5906dc82fd32541ae63901ba60e1e203e14dac8
simnJS <86597303+simnJS@users.noreply.github.com>
2022-06-23 16:55:12 +0200

    Update messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,6 +1,6 @@
 import { Event } from "sheweny";
 import type { ShewenyClient } from "sheweny";
-import {Message, TextChannel, MessageEmbed } from "discord.js";
+import { Message, TextChannel, MessageEmbed } from "discord.js";
 
 export class MessageCreateEvent extends Event {
   constructor(client: ShewenyClient) {
@@ -9,32 +9,34 @@ export class MessageCreateEvent extends Event {
     });
   }
 
-  async execute(message : Message) {
+  async execute(message: Message) {
     const settings = await this.client.db.get(message.guild!.id);
     const channel = await (message.guild!.channels.cache.find(c => c.id === settings.suggestChannel) as TextChannel)
 
     if (!channel) return;
     if (message.author.bot) return;
 
-    const threadAuthor = message.member!.displayName;
+    if (message.channel === channel) {
+      const threadAuthor = message.member!.displayName;
 
-    const suggestEmbed = new MessageEmbed()
+      const suggestEmbed = new MessageEmbed()
         .setColor('#FEE75C')
         .setTitle(`Nouvelle suggestion de ${message.author.username}`)
         .setDescription(message.content)
         .setTimestamp()
-    
 
-    await message.reply({embeds: [suggestEmbed]}).then(function (message) {
+
+      await message.reply({ embeds: [suggestEmbed] }).then(function (message) {
         message.react("👍")
         message.react("👎")
         message.startThread({
-            name: `${threadAuthor}-suggestion`,
-            autoArchiveDuration: 60,
+          name: `${threadAuthor}-suggestion`,
+          autoArchiveDuration: 60,
         });
-    });
+      });
 
-    message.delete();
-};
+      message.delete();
+    };
   }
+}
 
