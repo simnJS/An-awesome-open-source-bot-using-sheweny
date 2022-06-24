e6e7330cf5e8eecb3dc3de22dc25126e591ecb9a
simnJS <86597303+simnJS@users.noreply.github.com>
2022-06-24 10:19:11 +0200

    Rebuild warn system

--- src/commands/SlashCommands/moderation/warn.ts
+++ src/commands/SlashCommands/moderation/warn.ts
@@ -3,69 +3,154 @@ import { GuildMember, MessageEmbed, TextChannel } from "discord.js";
 import type { CommandInteraction } from "discord.js";
 import moment from "moment";
 export class WarnCommand extends Command {
-  constructor(client: ShewenyClient) {
-    super(client, {
-      name: "warn",
-      description: "Avertir un membre de la guild",
-      category: "Moderation",
-      type: "SLASH_COMMAND",
-      channel: "GUILD",
-      options: [
-        {
-          name: "user",
-          type: "USER",
-          description: "The user to warn",
-          required: true,
-        },
-        {
-          name: "reason",
-          description: "The reason of warn",
-          type: "STRING",
-          required: true,
-        },
-      ],
-      userPermissions: ["BAN_MEMBERS"],
-    });
-  }
-  async execute(interaction: CommandInteraction) {
-    
-    const settings = await this.client.db.get(interaction.guild!.id);
-    const member = interaction.options.getMember("user") as GuildMember;
-    if (!member)
-      return interaction.reply({
-        content: `User not found.`,
-        ephemeral: true,
-      });
+    constructor(client: ShewenyClient) {
+        super(client, {
+            name: "warn",
+            description: "Gérer le système de warn de la guild",
+            category: "Moderation",
+            type: "SLASH_COMMAND",
+            channel: "GUILD",
+            options: [
+                {
+                    name: "add",
+                    type: "SUB_COMMAND",
+                    description: "Ajouter un warn à une personne",
+                    options: [
+                        {
+                            name: "user",
+                            type: "USER",
+                            description: "The user to warn",
+                            required: true,
+                        },
+                        {
+                            name: "reason",
+                            description: "La raison du warn",
+                            type: "STRING",
+                            required: true,
+                        }
+                    ],
+                },
+                {
+                    name: "remove",
+                    description: "Retirer un warn à une personne",
+                    type: "SUB_COMMAND",
+                    options: [{
+                        name: "user_to_remove",
+                        type: "USER",
+                        description: "L'utilisateurou le warn doit être retirer",
+                        required: true,
+                    }, {
+                        name: "case",
+                        type: "NUMBER",
+                        description: "Le numéro du warn à retirer",
+                        required: true,
+                    }]
+                },
+                {
+                    name: "list",
+                    description: "Récupérer la list des warns d'une personne",
+                    type: "SUB_COMMAND",
+                    options: [{
+                        name: "utilisateur",
+                        type: "USER",
+                        description: "L'utilisateurou à chercher",
+                        required: true,
+                    },]
+                },
 
-    const reason: string =
-      interaction.options.getString("reason") || "No reason was provided.";
 
-    const embed = new MessageEmbed()
-        .setAuthor({name: member.user.tag, iconURL: member.user.displayAvatarURL()})
-        .setColor("#ff0000")
-        .setDescription(`**Membre**: ${member.user.tag} (${member.user.id})
+
+
+            ],
+            userPermissions: ["BAN_MEMBERS"],
+        });
+    }
+    async execute(interaction: CommandInteraction) {
+
+        const settings = await this.client.db.get(interaction.guildId!);
+        const option = interaction.options.getSubcommand(),
+            user_add = interaction.options.getMember("user") as GuildMember,
+            userToRemove = interaction.options.getMember("user_to_remove") as GuildMember,
+            Case = interaction.options.getNumber("case"),
+            utilisateur = interaction.options.getMember("utilisateur") as GuildMember,
+            reason = interaction.options.getString("reason");
+
+
+        if (option == "add") {
+
+            if (!user_add)
+                return interaction.reply({
+                    content: `User not found.`,
+                    ephemeral: true,
+                });
+
+            const embed = new MessageEmbed()
+                .setAuthor({ name: user_add.user.tag, iconURL: user_add.user.displayAvatarURL() })
+                .setColor("#ff0000")
+                .setDescription(`**Membre**: ${user_add.user.tag} (${user_add.user.id})
         **Action**: Warn
         **Raison**: ${reason}`);
 
-    const userArray = settings.users;  
+            const userArray = settings.users;
 
-    const user= {
-        case: (userArray.length + 1),
-        name: member.displayName,
-        id: member.id,
-        moderator: interaction.user.tag,
-        reason: reason,
-        date: moment().format("DD/MM/YYYY - HH:mm")
+            const user = {
+                case: (userArray.length + 1),
+                name: user_add.displayName,
+                id: user_add.id,
+                moderator: interaction.user.tag,
+                reason: reason,
+                date: moment().format("DD/MM/YYYY - HH:mm")
+            }
+
+            userArray.push(user);
+            await this.client.db.update(`${interaction.guild!.id}`, { users: userArray });
+            await interaction.reply({ content: `La commande warn a été éxécutée avec succès !`, embeds: [embed], ephemeral: true });
+
+            const logChannel = await (interaction.guild!.channels.cache.find(c => c.id === settings.modChannel) as TextChannel)
+
+            if (!interaction.channel) return;
+
+            await logChannel.send(`${interaction.user.username} a averti ${user_add.user.tag} pour la raison ${reason}`);
+
+        }
+
+        if (option == "remove") {
+            const settings = await this.client.db.get(interaction.guild!.id);
+
+            const filteredId = settings.users.map((u: any) => u.case).indexOf(Case);
+            if (filteredId == -1) return interaction.reply('Ce warn n\'existe pas !');
+
+            settings.users.splice(filteredId, 1);
+
+            await this.client.db.update(interaction.guild!.id, { users: settings.users });
+
+            await interaction.reply(`Le warn ${Case} a bien été supprimé.`);
+
+
+            const logChannel = await (interaction.guild!.channels.cache.find(c => c.id === settings.modChannel) as TextChannel)
+
+            if (!logChannel) return;
+
+            await logChannel.send(`${interaction.user.username} a supprimé le warn ${Case}`);
+        }
+
+        if (option == "list") {
+            if (!utilisateur)
+                return interaction.reply({
+                    content: `User not found.`,
+                    ephemeral: true,
+                });
+
+            const filteredUser = settings.users.filter((u: any) => u.id == utilisateur.id);
+            if (filteredUser.length == 0) return interaction.reply('Cette utilisateur n\'a pas de warns !');
+
+            let warnList = `Liste des warns pour \`${utilisateur.user.tag}\` (**${utilisateur.id}**) : \n`;
+
+            for (let warn of filteredUser) {
+                warnList += `\n**${warn.case}** - Par \`${warn.moderator}\` - (le ${warn.date}). Raison: \`${warn.reason}\``;
+            }
+
+            await interaction.reply(warnList);
+        }
     }
-
-    userArray.push(user);
-    await this.client.db.update(`${interaction.guild!.id}`, {users: userArray});
-    await interaction.reply({content : `La commande warn a été éxécutée avec succès !` , embeds : [embed] , ephemeral: true});
-
-    const logChannel = await (interaction.guild!.channels.cache.find(c => c.id === settings.modChannel) as TextChannel)
-
-    if (!interaction.channel) return;
-
-    await logChannel.send(`${interaction.user.username} a averti ${member.user.tag} pour la raison ${reason}`);
-  }
 }
