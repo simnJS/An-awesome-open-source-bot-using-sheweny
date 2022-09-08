5bd997d9c70ee3c5834ddeeb5fb725bf308843ce
simnJS <simon42400@gmail.com>
2022-09-08 22:10:19 +0200

    Update guildCreate.ts

--- src/events/guild/guildCreate.ts
+++ src/events/guild/guildCreate.ts
@@ -1,7 +1,6 @@
 import { Event } from "sheweny";
 import type { ShewenyClient } from "sheweny";
-import { Guild } from "discord.js";
-
+import { Guild, EmbedBuilder } from "discord.js";
 
 export class GuildEvent extends Event {
   constructor(client: ShewenyClient) {
@@ -11,8 +10,24 @@ export class GuildEvent extends Event {
   }
 
   async execute(guild: Guild) {
-    
     console.log(`New guild ! ${guild.name} (${guild.id})`);
     await this.client.db.post({ guildId: guild.id, name: guild.name });
+
+    const guildOwner = await guild.members.fetch(guild.ownerId);
+    const owner = guildOwner.user;
+
+    try {
+      const embed = new EmbedBuilder()
+        .setTitle("Merci d'avoir ajouté le bot !")
+        .setDescription(
+          "Pour configurer le bot, faites `/config + la configuration`"
+        )
+        .setColor("#00FF00")
+        .setTimestamp();
+
+      await owner.send({ embeds: [embed] });
+    } catch (error) {
+      console.log(error);
+    }
   }
-};
+}
